3ae3ff85e1ecc1b79f15bdae128007fba8546ab3
jace-wong <k_banh@icloud.com>
2023-10-25 16:00:54 -0600

    ahksduyagdikuaGDKIUSGKJHDgawjydgajsgdkuadgksaudgusagd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/MainOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/MainOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.OpMode;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -9,6 +10,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
+@Disabled
 
 public class MainOp extends LinearOpMode {
     @Override
